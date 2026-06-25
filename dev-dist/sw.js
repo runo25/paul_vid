5e362ab1b627434d6200085b587a00a3ce4fbe8d
ruonr <oawhie@gmail.com>
2026-06-25 23:45:05 +0100

    feat: implement service worker for asset precaching and navigation routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mmll2vv0cvo"
+    "revision": "0.l4g905opsfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
